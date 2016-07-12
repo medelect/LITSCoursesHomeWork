917d88636e75e5508bdd2d85ded18f7bd9f89118
alexGrut <medelect.axe@gmail.com>
2016-07-12 13:26:39 +0300

    tmp comm1

--- hw_1_2_checkParenthe.py
+++ hw_1_2_checkParenthe.py
@@ -1,13 +1,14 @@
 #######                 Sources                #######
-str0 = '((((((((((((((2, 3)))))))))))))'
-str1 = '((((((((((((((2, 3)))))))))))})'
-str2 = '(([{(((((((((((2, 3)))))))))))))'
-str3 = '{(((((((((((((2, 3))))))))))))}'
-str4 = '(()3())'
+strAll = ['((((((((((((((2, 3)))))))))))))'
+	'((((((((((((((2, 3)))))))))))})',
+	'(([{(((((((((((2, 3)))))))))))))',
+	'{(((((((((((((2, 3))))))))))))}',
+	'(()3())'
+	    ]
 #######                 Work Siquece           #######
 globDict= {'(':[[], [],')'],')':[[], [],'('],
            '{':[[], [],'}'],'}':[[], [],'{'],
-           '[':[[], [],']'],']':[[], [],'[']
+           '[':[[], [],']'],']':[[], [],'[']											#10
        }
 #######     Chack symbol of parenthesis    #######
 def checks(var):
@@ -17,28 +18,27 @@ def checks(var):
         return False
 #######                 Type of error             #######
 def errMaker(varDict):
-    for parenthe in varDict:
-        print parenthe , varDict[parenthe]
+    for parenthe in varDict:													#20
+        print (parenthe , varDict[parenthe])
         #for count in varDict[parenthe]:
          #   print varDict[parenthe][0], varDict[parenthe][1]
 
        #1 #if len(varDict[parenthe][0]) != len(varDict[varDict[parenthe][3]][0]) # erorr in pair parenthe
-       
 
 
        #2#openParenthe = '({['
           #closeParenthe = ')}]'
-          #if parenthe in openParenthe and varDict[varDict[parenthe][3]][1][0] > 
+          #if parenthe in openParenthe and varDict[varDict[parenthe][3]][1][0] > 						#30
 
        #3 #if varDict[parenthe][0]) == varDict[varDict[parenthe][3]][0][::-1] # erorr in siquence parenthe
 #########        fill      #######
 def fill(myDict):
     counterIncrement = 0
-    counterDecrement = len(str4)+1
-    print len(str4)
-    for item in str4:
+    counterDecrement = len(strAll[4])+1
+    print (len(strAll[4]))
+    for item in strAll[4]:
         counterIncrement += 1
-        counterDecrement -= 1        
+        counterDecrement -= 1													#40
         if checks(item):
             myDict[item][0].append(counterIncrement)
             myDict[item][1].append(counterDecrement)
@@ -48,4 +48,4 @@ def fill(myDict):
 fill(globDict)
 errMaker(globDict)
 
-
+																#50
