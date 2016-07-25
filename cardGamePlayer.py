47a36d58163ecf005b14b797c8139e4de8817407
alexGrut <medelect.axe@gmail.com>
2016-07-25 18:53:56 +0300

    GAME FINISHEDpython3 ./cardGamePlayer.py !

--- cardGamePlayer.py
+++ cardGamePlayer.py
@@ -2,45 +2,36 @@ from cardgame import GameBatch
 import os
 
 class GamePlayer:
-    def __init__(self):
-        self.playerName
-        self.playerSum
+    def __init__(self,name, batch):
+        self.batch = batch
+        self.playerName = name
+        self.playerSum = 0
+
+    def play(self):
+        print("ALL SUM", self.playerName ,"IS ", self.playerSum )
+        ask = input("Do you want extra card press y/n " )
+        if ask == 'y':
+            obj = self.batch.getCard()
+            self.batch.printCard(obj)
+            self.playerSum += obj.coast
+            return False
+        elif ask == "n":
+            return True
 
 if __name__ == "__main__":
     user = GameBatch()
-    sumCoins = 0 
-    obj = user.getCard()
-    user.printCard(obj)
-    sumCoins += obj.coast
-    print("ALL SUM IS ", sumCoins )
-    ask = input("Do you whant extra card press y/n")
-    if ask == 'y':
-        obj = user.getCard()
-        user.printCard(obj)
-        sumCoins += obj.coast
-    print("ALL SUM IS ", sumCoins )
-    ask = input("Do you whant extra card press y/n")
-    if ask == 'y':
-        obj = user.getCard()
-        user.printCard(obj)
-        sumCoins += obj.coast
-    print("ALL SUM IS ", sumCoins )
-    ask = input("Do you whant extra card press y/n")
-    if ask == 'y':
-        obj = user.getCard()
-        user.printCard(obj)
-        sumCoins += obj.coast
-    print("ALL SUM IS ", sumCoins )
-    ask = input("Do you whant extra card press y/n")
-    if ask == 'y':
-        obj = user.getCard()
-        user.printCard(obj)
-        sumCoins += obj.coast
-    print("ALL SUM IS ", sumCoins )
-
-
-
-
+    party = [GamePlayer("Bob",user), GamePlayer("Mark", user), GamePlayer("Brad",user)]
+    for item in party:
+        while True:
+            if item.playerSum < 22:
+                if item.play():
+                    break
+            else:
+                print(item.playerName, " you LOSE")
+                break
+    for item in party:
+        print(item.playerName, " Have ", item.playerSum)
+    print("Greating WINNER!!!")
 
 
 #os.system('clear')
